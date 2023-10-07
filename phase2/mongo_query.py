ef315465ee805369f34c0728b8a7f15fb11ea1c2
Gaurav Bansode <gauravbansode90@gmail.com>
2023-10-06 18:59:32 -0700

    Added a query_all for collection_name

--- phase2/mongo_query.py
+++ phase2/mongo_query.py
@@ -1,10 +1,24 @@
 import mongo_connection
 from bson.json_util import dumps
 
-def query(collection_name, imageID):
+def get_collection(collection_name):
     db = mongo_connection.get_database()
     collection = db[collection_name]
-    cursor = collection.find({"imageID": imageID})
+    return collection
+
+def convert_to_json(cursor):
     list_cur = list(cursor)
-    json_data = dumps(list_cur, indent = 2) 
+    json_data = dumps(list_cur, indent = 2)
+    return json_data
+
+def query(collection_name, imageID):
+    collection = get_collection(collection_name)
+    cursor = collection.find({"imageID": imageID})
+    json_data = convert_to_json(cursor)
+    return json_data
+
+def query_all(collection_name):
+    collection = get_collection(collection_name)
+    cursor = collection.find()
+    json_data = convert_to_json(cursor)
     return json_data
