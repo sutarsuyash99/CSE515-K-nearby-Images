4864bf52b046bab6de79d3475698a2e1be3af352
parshva.barbhaya@gmail.com <parshva.barbhaya@somaiya.edu>
2023-10-10 01:37:13 -0700

    fix: added fix for latent dirichet allocation

--- phase2/dimension_reduction.py
+++ phase2/dimension_reduction.py
@@ -1,21 +1,25 @@
 # Add all the dimension reduction algotihms here
 import numpy as np
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.decomposition import LatentDirichletAllocation as LDA
 
-def lda(k: int, data_collection: np.ndarray, data_label: list) -> np.ndarray:
+def lda(k: int, data_collection: np.ndarray) -> np.ndarray:
     '''
     returns reduced matrix using sklearn's LDA inbuilt function
-    since LDA is supervised learning, we need data_label input size
-    source code reference: https://scikit-learn.org/0.20/modules/generated/sklearn.discriminant_analysis.LinearDiscriminantAnalysis.html#sklearn.discriminant_analysis.LinearDiscriminantAnalysis.fit
-    inbuilt-method: https://github.com/scikit-learn/scikit-learn/blob/be7633dbe/sklearn/base.py#L440
+    source code reference: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.LatentDirichletAllocation.html#sklearn.decomposition.LatentDirichletAllocation
+    inbuilt-method: https://github.com/scikit-learn/scikit-learn/blob/d99b728b3/sklearn/base.py#L888
+    explanation: https://scikit-learn.org/stable/modules/decomposition.html#latentdirichletallocation
     '''
     # converting data_collection from multi dimensions to 2 dimensions
     if data_collection.ndim >= 2:
         data_collection = data_collection.flatten()
     
-    if data_collection.shape[0] == len(data_label):
-        lda_model = LDA(solver='svd', n_components=k)
-        reduced_data = lda_model.fit_transform(data_collection, data_label)
-        print(f'Reducing {data_collection.shape} => {reduced_data.shape}')
-        return reduced_data
-    else: raise ValueError
+    # there is something weird, for k = 1, currently raising error
+    if k == 1: 
+        # every value comes out as [1.]
+        raise ValueError
+
+    lda_model = LDA(n_components=k, max_iter=10, random_state=42, learning_method='batch')
+    # print(train_data.shape[0] == len(train_label))
+    reduced_data = lda_model.fit_transform(data_collection)
+    print(f'Reducing {data_collection.shape} => {reduced_data.shape}')
+    return reduced_data
