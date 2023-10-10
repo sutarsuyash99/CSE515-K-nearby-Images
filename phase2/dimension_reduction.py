13a8d33fe5440b4d5f9226843ab248d2241e5c2e
Hardik-hp <hpatel7067@gmail.com>
2023-10-10 02:21:34 -0700

    Added Task3 and NMF code

--- phase2/dimension_reduction.py
+++ phase2/dimension_reduction.py
@@ -1,1 +1,47 @@
 # Add all the dimension reduction algotihms here
+import numpy as np
+import torch
+from tqdm import tqdm
+
+
+def nmf_als(V, K, iteration=200, tol=1, alpha=0.01):
+    """
+    Non-negative Matrix Factorization (NMF) using Alternating Least Squares (ALS) method.
+
+    Parameters:
+        V (numpy.ndarray): The input non-negative data matrix of shape (m, n).
+        K (int): Desired number of latent semantics
+        max_iter (int): Maximum number of iterations.
+        tol (float): Tolerance to stop the iterations.
+        alpha (float): Regularization parameter.
+
+    Returns:
+        W (numpy.ndarray): The factorization matrix of shape (m, rank).
+        H (numpy.ndarray): The factorization matrix of shape (rank, n).
+    """
+    # Shape of the original vector
+    m, n = V.shape
+
+    # Initialize W and H with random non-negative values
+    np.random.seed(0)
+    W = np.random.rand(m, K)
+    H = np.random.rand(K, n)
+
+    for iter in tqdm(range(iteration)):
+        # Update H 
+        H = H * (np.dot(W.T, V)) / (np.dot(np.dot(W.T, W), H))
+        # Update W 
+        W = W * (np.dot(V, H.T) ) / (np.dot(W, np.dot(H, H.T)) )
+
+        # Error or distance from the original matrix , NMF for very large matrix have distance > 300
+        residual = np.linalg.norm(V - np.dot(W, H))
+        
+
+        # Check for convergence
+        if residual < tol:
+            break
+    print(residual)
+    return W, H
+
+
+ 
