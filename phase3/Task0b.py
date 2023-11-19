389bdb9948baab7f9bce67938a5a933f97dd438b
parshva.barbhaya@gmail.com <parshva.barbhaya@somaiya.edu>
2023-11-19 13:30:42 -0700

    skeleton for task0a/0b

---
+++ phase3/Task0b.py
@@ -0,0 +1,30 @@
+import Mongo.mongo_query_np as mongo_query
+import utils
+import inherent_dimensionality
+
+class Task0b:
+    def __init__(self) -> None:
+        self.dataset, self.labelled_images = utils.initialise_project()
+
+    def all_data(self):
+        # Assumption - using FC layer
+        int_model_option = 5
+        model_option = utils.feature_model[int_model_option]
+        # Load all data
+        for i in range(len(self.labelled_images)):
+            # cur_index = i
+            label_selected = self.dataset.categories[i]
+            print(f"Input provided: {i} => {label_selected}")
+            # get all data for label
+            label_images = mongo_query.get_all_feature_descriptor_for_label(model_option, label_selected)
+            # call functions -- call here in the below format
+            k = inherent_dimensionality.test(label_images)
+            # print inherent dimensionality    
+            print(f"For Label: {label_selected} at index: {i}, inherent dimensionality: {k}")
+        # call functions -- call here in the below format
+        # k = inherent_dimensionality.fn()
+        # print inherent dimensionality
+
+if __name__ == '__main__':
+    task0b = Task0b()
+    task0b.all_data()
--- phase3/startup.sh
+++ phase3/startup.sh
@@ -1,3 +1,2 @@
 python3 -m venv final && source final/bin/activate
 pip install -r requirements.txt
-python3 main.py
